856d363a53ac12d34ae44772d7ea3a8a459100d0
Alan Nguyen <tuan8101@gmail.com>
2020-05-02 10:49:53 -0600

    Pass addTrack to SearchResults:

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -50,6 +50,7 @@ class App extends React.Component {
         },
       ],
     };
+    this.addTrack = this.addTrack.bind(this);
   }
 
   addTrack(track) {
@@ -71,7 +72,10 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
