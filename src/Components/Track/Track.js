6e835de8977c502a81a4258ec0fb2166162e21e9
Alan Nguyen <tuan8101@gmail.com>
2020-05-02 07:32:30 -0600

    Create render Action method in Track

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action">-</button>;
+    } else {
+      return <button className="Track-action">+</button>;
+    }
+  }
+
   render() {
     return (
       <div className="Track">
