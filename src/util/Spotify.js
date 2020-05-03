b9c55024f268931aed36421fe4de6cb27fcfe6a8
Alan Nguyen <tuan8101@gmail.com>
2020-05-03 13:13:16 -0600

    Fix bugs in Spotify

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -14,7 +14,7 @@ const Spotify = {
 
     if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
-      const expires_in = Number(expiresInMatch[1]);
+      const expiresIn = Number(expiresInMatch[1]);
       // This clears the parameters, allowing us to grab a new access token when it expires
       window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
