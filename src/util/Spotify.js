ef3a5ce5244a3ead6d0c11037b2a8edec31e61e1
Alan Nguyen <tuan8101@gmail.com>
2020-05-02 23:18:47 -0600

    Check for access token match

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -6,6 +6,9 @@ const Spotify = {
     if (accessToken) {
       return accessToken;
     }
+    // Check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
   },
 };
 
