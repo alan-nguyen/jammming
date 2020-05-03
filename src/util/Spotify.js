7da5c62c3f0ff52e47ff6463f9e4bfd3a54c3a24
Alan Nguyen <tuan8101@gmail.com>
2020-05-03 11:03:13 -0600

    Add clientId and redirectUri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+const clientId = '1c20b29457b94f69bc70b14d677499dc';
+const redirectUri = 'http://localhost:3000/';
 let accessToken = '';
 
 // Spotify module
@@ -13,6 +15,10 @@ const Spotify = {
     if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
       const expires_in = Number(expiresInMatch[1]);
+      // This clears the parameters, allowing us to grab a new access token when it expires
+      window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
     }
   },
 };
